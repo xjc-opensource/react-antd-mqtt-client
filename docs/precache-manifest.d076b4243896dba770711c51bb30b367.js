96e6e4de1f23bc2108508af430ce81c6ead44197
ben <ben@opensource.ext>
2019-06-17 18:11:15 +0800

    +docs
    Signed-off-by: ben <ben@opensource.ext>

---
+++ docs/precache-manifest.d076b4243896dba770711c51bb30b367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec77f17420ad5f0a1493e1666f9c3d25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c844983fe76488792b2",
+    "url": "./static/css/2.a5e6fd64.chunk.css"
+  },
+  {
+    "revision": "d05c168ca3dda82474d6",
+    "url": "./static/css/main.d5938570.chunk.css"
+  },
+  {
+    "revision": "1c844983fe76488792b2",
+    "url": "./static/js/2.2cffb252.chunk.js"
+  },
+  {
+    "revision": "d05c168ca3dda82474d6",
+    "url": "./static/js/main.5fdf5771.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
